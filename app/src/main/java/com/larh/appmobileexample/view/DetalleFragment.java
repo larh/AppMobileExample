f87b77eed726ff1a1e90b1621f8e6e8522305ab1
luis.rosales <larh0510@gmail.com>
2019-05-25 12:13:20 -0500

    Ejemplos

--- app/src/main/java/com/larh/appmobileexample/view/DetalleFragment.java
+++ app/src/main/java/com/larh/appmobileexample/view/DetalleFragment.java
@@ -4,31 +4,39 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.larh.appmobileexample.R;
+import com.larh.appmobileexample.view.adapter.ImagePageAdapter;
 import com.larh.appmobileexample.view.listener.OnFragmentInteractionListener;
 
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link DetalleFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link DetalleFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DetalleFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    @BindView(R.id.viewPager)
+    ViewPager viewPager;
+    @BindView(R.id.layoutDots)
+    LinearLayout layoutDots;
+    private TextView[] dots;
+
+    List<String> imagenes;
 
     private OnFragmentInteractionListener mListener;
 
@@ -36,15 +44,6 @@ public class DetalleFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DetalleFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static DetalleFragment newInstance(String param1, String param2) {
         DetalleFragment fragment = new DetalleFragment();
         Bundle args = new Bundle();
@@ -66,11 +65,63 @@ public class DetalleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detalle, container, false);
+        View view = inflater.inflate(R.layout.fragment_detalle, container, false);
+        ButterKnife.bind(this,view);
+
+        imagenes = new ArrayList<>();
+        imagenes.add("https://diccionarioactual.com/wp-content/uploads/2016/05/Tux-768x788.png");
+        imagenes.add("https://www.gnu.org/graphics/babies/GnuTux.jpg");
+
+        iniciarSlider(view, imagenes);
+
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+    private void iniciarSlider(View view, List<String> imagenes){
+        ImagePageAdapter imagePageAdapter = new ImagePageAdapter(view.getContext(), imagenes);
+        viewPager.setAdapter(imagePageAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+        viewPager.setOffscreenPageLimit(imagenes.size());
+        addBottomDots(0);
+    }
+
+    private void addBottomDots(int currentPage) {
+        dots = new TextView[imagenes.size()];
+
+        layoutDots.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(getContext());
+            dots[i].setText(Html.fromHtml("&#9675;"));
+            dots[i].setTextSize(15);
+            dots[i].setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+            layoutDots.addView(dots[i]);
+        }
+
+        if (dots.length > 0){
+            dots[currentPage].setText(Html.fromHtml("&#9679;"));
+            dots[currentPage].setTextSize(15);
+            dots[currentPage].setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+    }
+
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageSelected(int position) {
+            addBottomDots(position);
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+    };
+
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(this);
